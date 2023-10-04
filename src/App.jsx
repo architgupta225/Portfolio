2e5eb29ea4c227b1b3c2837c198c4ba61e567ca9
Architgupta225 <architgupta225@gmail.com>
2023-10-05 02:07:15 +0530

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import About from './pages/about/About'
 import Portfolio from './pages/portfolio/Portfolio'
 import Contact from './pages/contact/Contact'
 import Navbar from './components/Navbar'
+import Themes from './components/Themes'
 
 function App() {
 
   return (
   <BrowserRouter> 
   <Navbar />
+  <Themes />
   <Routes>
     <Route index element={<Home />} />
     <Route path='about' element={<About />} />
